c78462fa506e8402cbcf062ccee5b3d6f47afce7
Panwa Muangsong <panwa1379@gmail.com>
2023-05-15 13:42:52 +0700

    fix import

--- server/src/database/init.model.ts
+++ server/src/database/init.model.ts
@@ -2,20 +2,15 @@ import { Dialect, Sequelize } from 'sequelize';
 import yn from 'yn';
 import debug from 'debug';
 import config from '../config/global.config';
-import { initUserModel } from './models/users.model';
-import { initUserTypeModel } from './models/user_types.model';
-import { initRoomModel } from './models/rooms.model';
+import UserModel, { initUserModel } from './models/users.model';
+import UserTypeModel, { initUserTypeModel } from './models/user_types.model';
+import RoomModel, { initRoomModel } from './models/rooms.model';
 import ReserveModel, { initReserveModel } from './models/reserve.model';
+import EquipmentModel, { initEquipmentModel } from './models/equipments.model';
 import { initNewsModel } from './models/news.model';
-import { initEquipmentModel } from './models/equipments.model';
 import { initGlobalEquipmentModel } from './models/global_equipments.model';
 import { initAdminModel } from './models/admins.model';
 import { initSuperAdminModel } from './models/super_admins.model';
-
-import UserModel from './models/users.model';
-import UserTypeModel from './models/user_types.model';
-import RoomModel from './models/rooms.model';
-import EquipmentModel from './models/equipments.model';
 
 import SuperAdminController from '../controllers/super_admin.controller';
 import log from '../tools/log';
