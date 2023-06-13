057ff7d0f7248bbe46fdde5bc83c66909dca93ad
Panwa Muangsong <panwa1379@gmail.com>
2023-06-13 21:11:58 +0700

    add user types controller

---
+++ server/src/controllers/user_types.controller.ts
@@ -0,0 +1,56 @@
+import Sequelize, { Op } from 'sequelize';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import UserTypeModel, { UserTypeAttribute } from '../database/models/user_types.model';
+import config from '../config/global.config';
+import { log } from '../tools/log';
+
+class UserTypesController {
+    public static async getByID(id: string) {
+        return UserTypeModel.findOne({
+            where: {
+                id: id,
+            },
+            raw: true
+        });
+    }
+
+    public static async getAllUserTypes(limit: number, offset: number) {
+        return UserTypeModel.findAndCountAll({
+            // where: { available_status: true },
+            limit,
+            offset,
+            raw: true
+        });
+    }
+    public static async getAllUserTypesRaw() {
+        return UserTypeModel.findAndCountAll({ raw: true });
+    }
+
+    public static async createUserType(data: any) {
+        const packet: UserTypeAttribute = {
+            name: data.name,
+        };
+
+        return UserTypeModel.create(packet)
+            .then(() => true)
+            .catch((e) => {
+                log(e);
+                return false;
+            });
+    }
+
+    public static async update(data: any) {
+        return UserTypeModel.update({
+            name: data.name,
+        }, {
+            where: {
+                id: data.id,
+            },
+        }).then(() => true)
+            .catch(() => false);
+    }
+
+}
+
+export default UserTypesController;
