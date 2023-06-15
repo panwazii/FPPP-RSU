0b0ff09a81d24969399b66d7b75db50a56ede6d7
Panwa Muangsong <panwa1379@gmail.com>
2023-06-15 16:04:39 +0700

    fix rooms in fetch

--- client/components/index/Room.vue
+++ client/components/index/Room.vue
@@ -1,17 +1,17 @@
 <template>
-          <v-card class="mx-0 ml-1 mr-1" width="250" height="250">
-            <v-img :src="picture" height="100px"></v-img>
-            <v-card-title> {{ name }}</v-card-title>
-            <!-- <v-card-subtitle> {{ room.subtitle }} </v-card-subtitle> -->
-            <v-card-actions>
-              <v-btn color="orange lighten-2" text> Explore </v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
+  <v-card class="mx-0 ml-1 mr-1" width="250" height="250">
+    <v-img :src="picture" height="100px"></v-img>
+    <v-card-title> {{ name }}</v-card-title>
+    <!-- <v-card-subtitle> {{ room.subtitle }} </v-card-subtitle> -->
+    <v-card-actions>
+      <v-btn color="orange lighten-2" text> Explore </v-btn>
+      <v-spacer></v-spacer>
+    </v-card-actions>
+  </v-card>
 </template>
 <script>
 export default {
-    props: {
+  props: {
     picture: {
       type: String,
       default: () => '',
@@ -20,6 +20,6 @@ export default {
       type: String,
       default: () => '',
     },
-  }
+  },
 }
 </script>
