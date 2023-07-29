f44af2a10b8b0f1431991a9d5f8f4bfb0a163e5c
SatorJP <75246985+mommore@users.noreply.github.com>
2023-07-29 16:40:45 +0700

    prepare idGenerate

--- server/src/tools/util.ts
+++ server/src/tools/util.ts
@@ -57,3 +57,19 @@ export function uploadSinglePicture(fileorinalname: any, mimetype: any, buffer: 
     })
 
 }
+
+export function idGenerate(x: any, val: any) {
+    try {
+        if (x === null) {
+            return val
+        }
+
+        const y = Number(x)
+        if (!Number.isNaN(y)) {
+            return Number(y)
+        } else {
+            return val
+        }
+    }
+    catch (error) { console.log(error); }
+}
