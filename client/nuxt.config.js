21124068b71f0120dd124dc4b59b5b6d7728e6dd
Panwa Muangsong <panwa1379@gmail.com>
2023-11-02 15:35:29 +0700

    add env axios port

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -51,7 +51,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8080/',
+    baseURL: process.env.BASE_URL,
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
