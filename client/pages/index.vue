f53e716a8611b7a7bf2948eac037cc4569adebe5
Panwa Muangsong <panwa1379@gmail.com>
2023-11-03 05:49:47 +0700

    fix merg

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,37 +1,16 @@
 <template>
   <div>
-<<<<<<< HEAD
-    
-    <section>
-      <div class="container">
-        <div class="left">
-          <h1 class="center">FOOD PROCESS PILOT PLANT</h1>
-        </div>
-        <div class="right"></div>
-=======
+    <!-- <h1>{{ $store.getters.getCartItems }}</h1>
+    <v-btn @click="$store.dispatch('addCartItems', { id: 1, name: 'test1' })">cart add</v-btn>
+    <v-btn @click="$store.dispatch('removeCartItems', 1)">cart remove</v-btn> -->
     <v-parallax height="750" :src="require('~/static/img/index/index-bg.jpg')">
       <div
         class="d-flex flex-column fill-height justify-center align-center text-white"
       >
         <h1 class="text-h4 font-weight-bold mb-4 paralax-text"></h1>
->>>>>>> 1793cb012f4485ab77c24a5958bc130087c4e416
       </div>
     </v-parallax>
     <indexService />
-<<<<<<< HEAD
-    <!-- <h1>{{ $store.getters.getCartItems }}</h1>
-    <v-btn @click="$store.dispatch('addCartItems', { id: 1, name: 'test1' })">cart add</v-btn>
-    <v-btn @click="$store.dispatch('removeCartItems', 1)">cart remove</v-btn> -->
-    <section>
-      <v-img class="overlay" width="600" :src="image" />
-      <v-row class="justify-center">
-        <span class="roomcenter">
-          <h1>ROOM</h1>
-        </span>
-        <v-col v-for="(rooms, index) in room.slice(0, 3)" :key="rooms.name">
-          <indexRoomIndex :id="rooms.id" :picture="rooms.Picture[0].url" :name="rooms.name" :detail="rooms.details" />
-        </v-col>
-=======
     <indexAbout />
 
     <h1 class="text-h4 font-weight-bold mb-4 d-flex justify-center">
@@ -45,7 +24,6 @@
     <div class="room1 justify-center">
       <v-row class="justify-center mt-12">
         <indexAboutUs />
->>>>>>> 1793cb012f4485ab77c24a5958bc130087c4e416
       </v-row>
     </div>
 
@@ -98,49 +76,9 @@ export default {
 .paralax-text {
   text-shadow: 2px 2px #000000;
 }
-<<<<<<< HEAD
-
-section {
-  min-height: 100vh;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #ffffff;
-  position: relative;
-}
-
-a {
-  height: 35px;
-  width: 200px;
-  text-align: center;
-  text-decoration: none;
-  text-transform: uppercase;
-  background-color: rgb(106, 95, 73);
-  color: rgb(255, 255, 255);
-  padding: 5px 20px;
-  display: inline-block;
-  letter-spacing: 2px;
-  border-radius: 200px;
-  position: absolute;
-  bottom: 200px;
-  font-size: 18px;
-  z-index: 1;
-}
-
-.container {
-  width: 90%;
-  max-width: 1000px;
-  margin: 50px auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  /* background-color: aqua; */
-=======
 .room1 {
   max-width: 1200px;
   margin: auto;
->>>>>>> 1793cb012f4485ab77c24a5958bc130087c4e416
 }
 
 .roomcenter {
@@ -151,56 +89,6 @@ a {
   color: rgb(106, 95, 73);
   z-index: 1;
 }
-<<<<<<< HEAD
-
-.center {
-  padding: 180px 0;
-  font-family: Tahoma (sans-serif);
-  font-size: 40px;
-  text-align: center;
-}
-
-.left {
-  width: 50%;
-  height: 600px;
-  text-align: center;
-  color: rgb(106, 95, 73);
-  background-color: #ffffff;
-  border-radius: 8px;
-}
-
-.right {
-  width: 200%;
-  height: 800px;
-  background-image: url(~/static/img/index/872.png);
-  background-repeat: no-repeat;
-  background-size: cover;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 30px;
-  color: white;
-  margin-left: 50px;
-}
-
-@media only screen and (max-width: 768px) {
-  .container {
-    flex-direction: column;
-    width: 100%;
-    margin: 0 20px;
-  }
-
-  .left {
-    width: 100%;
-    height: 400px;
-  }
-
-  .right {
-    width: 90%;
-    margin: 0;
-    margin-top: -100px;
-  }
-=======
 a {
   height: 50px;
   width: 200px;
@@ -214,6 +102,5 @@ a {
   letter-spacing: 2px;
   border-radius: 200px;
   justify-items: center;
->>>>>>> 1793cb012f4485ab77c24a5958bc130087c4e416
 }
 </style>
