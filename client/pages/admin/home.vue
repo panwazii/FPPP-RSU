60cbb4b64d526289f6bd551d5a95664bd53c0e7b
Panwa Muangsong <panwa1379@gmail.com>
2023-11-22 16:33:59 +0700

    add charts

--- client/pages/admin/home.vue
+++ client/pages/admin/home.vue
@@ -1,6 +1,12 @@
 <template>
-  <v-container>
-  </v-container>
+  <div id="chart">
+    <apexchart
+      type="line"
+      height="350"
+      :options="chart.chartOptions"
+      :series="chart.series"
+    ></apexchart>
+  </div>
 </template>
 <script>
 export default {
@@ -11,11 +17,61 @@ export default {
       title: 'admin home',
     }
   },
+  components: {
+    [process.browser && 'apexchart']: () => import('vue-apexcharts'),
+  },
   data() {
-    return {}
+    return {
+      chart: {
+        series: [
+          {
+            name: 'Desktops',
+            data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+          },
+        ],
+        chartOptions: {
+          chart: {
+            height: 350,
+            type: 'line',
+            zoom: {
+              enabled: false,
+            },
+          },
+          dataLabels: {
+            enabled: false,
+          },
+          stroke: {
+            curve: 'straight',
+          },
+          title: {
+            text: 'Product Trends by Month',
+            align: 'left',
+          },
+          // grid: {
+          //   row: {
+          //     colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+          //     opacity: 0.5,
+          //   },
+          // },
+          xaxis: {
+            categories: [
+              'Jan',
+              'Feb',
+              'Mar',
+              'Apr',
+              'May',
+              'Jun',
+              'Jul',
+              'Aug',
+              'Sep',
+            ],
+          },
+        },
+      },
+    }
   },
   mounted() {
-    this.$store.dispatch('setPathName',"home")
+    this.$store.dispatch('setPathName', 'home')
   },
 }
 </script>
